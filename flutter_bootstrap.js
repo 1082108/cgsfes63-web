5ae7faa855ee148795f7e588ec5101f7e2b8b3cf
1082108 <stuomuiya@gmail.com>
2025-11-01 13:45:52 +0000

    Deploying to gh-pages from @ 1082108/cgsfes63-web@1bb0fb7e6dd0218f8e58a5e1be2cef2c2bd6d48d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "48225014"
+    serviceWorkerVersion: "2578788437"
   }
 });
